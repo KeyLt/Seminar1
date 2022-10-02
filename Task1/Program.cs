979a4cf048f94a70d1f183d8a6bce627aa2b07be
Your Name <you@example.com>
2022-10-02 21:22:51 +0300

    Task1_2_3

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число ");
+string First_number = Console.ReadLine();
+int number_first = Convert.ToInt32(First_number);
+Console.Write("Введите второе число ");
+string Second_number = Console.ReadLine();
+int number_second = Convert.ToInt32(Second_number);
+int Numbers_Task = 0;
+Numbers_Task = number_first / number_second;
+if(Numbers_Task == number_second)
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго числа");
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿int Monday = 1;
+int Tuesday = 2;
+int Wednesday = 3;
+int Thusrsday = 4;
+int Friday = 5;
+int Saturday = 6;
+int Sunday = 7;
+
+Console.Write("Введите число ");
+string Number = Console.ReadLine();
+int numbers = Convert.ToInt32(Number);
+ if ( numbers == Monday) Console.WriteLine("Monday");
+ if ( numbers == Tuesday) Console.WriteLine("Tuesday");
+ if ( numbers == Wednesday) Console.WriteLine("Wednesday");
+ if ( numbers == Thusrsday) Console.WriteLine("Thusrsday");
+ if ( numbers == Friday) Console.WriteLine("Friday");
+ if ( numbers == Saturday) Console.WriteLine("Saturday");
+ if ( numbers == Sunday) Console.WriteLine("Sunday");
